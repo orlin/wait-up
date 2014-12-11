1be3dccb984028c1968fd7b27048b83b5924f786
Orlin M Bozhinov <om@soundsapiens.com>
2014-12-11 18:47:36 +0200

    wait-up for localhost

---
+++ bin.coffee
@@ -0,0 +1,34 @@
+#!/usr/bin/env coffee
+
+wait = require("wait")
+request = require("request")
+
+patience = 1000 # how long request will wait (timeout)
+spacings = 100 # time between retries
+duration = 42000 # the ultimate patience of a process
+opts = method: "GET", uri: "http://localhost/", timeout: patience
+
+exit = (code, res) ->
+  if res? then console.log "Got status #{res?.statusCode}"
+  process.exit code
+
+console.log opts.method + ' ' + opts.uri
+waiting = false
+
+wait.doAndRepeat 100, ->
+  request.get opts, (err, res) ->
+    if !err
+      console.log() if waiting
+      if res.statusCode is 200
+        exit 0, res
+      else
+        exit 1, res
+    else
+      # isn't up yet
+      waiting = true
+      process.stdout.write '.'
+
+wait.wait duration, ->
+  console.log() if waiting
+  console.log "Giving-up after #{duration} ms"
+  exit 1
---
+++ bin.js
@@ -0,0 +1,54 @@
+var duration, exit, opts, patience, request, spacings, wait, waiting;
+
+wait = require("wait");
+
+request = require("request");
+
+patience = 1000;
+
+spacings = 100;
+
+duration = 42000;
+
+opts = {
+  method: "GET",
+  uri: "http://localhost/",
+  timeout: patience
+};
+
+exit = function(code, res) {
+  if (res != null) {
+    console.log("Got status " + (res != null ? res.statusCode : void 0));
+  }
+  return process.exit(code);
+};
+
+console.log(opts.method + ' ' + opts.uri);
+
+waiting = false;
+
+wait.doAndRepeat(100, function() {
+  return request.get(opts, function(err, res) {
+    if (!err) {
+      if (waiting) {
+        console.log();
+      }
+      if (res.statusCode === 200) {
+        return exit(0, res);
+      } else {
+        return exit(1, res);
+      }
+    } else {
+      waiting = true;
+      return process.stdout.write('.');
+    }
+  });
+});
+
+wait.wait(duration, function() {
+  if (waiting) {
+    console.log();
+  }
+  console.log("Giving-up after " + duration + " ms");
+  return exit(1);
+});
